9a61f175f704ad10329e8a4b9f52ca22da527eed
Johnny Rice <jrice@labact.com>
2020-06-30 03:01:09 -0400

    Provide challenge 05

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-module.exports = function nameThisFunction(arr) {
-    if (arr.length === 0) return 0
+// We are using the `%` Remainder operator
+// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators#Remainder
+// and https://www.mathsisfun.com/definitions/modulo-operation.html
+module.exports = function function nameThisFunction(x, y) {
+    while (y) {
+        const t = y
+        y = x % y
+        x = t
+    }
 
-    const [first, ...rest] = arr
-
-    // We have Recursion here. 
-    // For more info see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions#Recursion
-    return first + nameThisFunction(rest)
+    return x
 }
-
