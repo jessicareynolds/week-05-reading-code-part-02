5e1133a15da9adffaeac765a5121518d7d8ba1e0
Johnny Rice <jrice@labact.com>
2020-06-30 03:13:14 -0400

    Provide challenge 08

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/shift
-module.exports = function nameThisFunction(val1, val2) {
-    let newList = [];
-
-    while (val1.length && val2.length) {
-        if (val1[0] <= val2[0]) {
-            newList.push(val1.shift());
-        } else {
-            newList.push(val2.shift());
-        }
-    }
-
-    return newList.concat(val1).concat(val2);
-};
+//
+// You've completed all that we have here. 
+// You have leveled up!
+// Keep Going! See https://leetcode.com/problemset/all/
+//
